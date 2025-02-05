e557b9e73b022e7b2ed71a4be74a8800ea0839e2
morteza rasooli <motteza1182@gmail.com>
2025-02-05 15:46:39 +0330

    first commit

---
+++ manage_bot.py
@@ -0,0 +1,76 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+
+TOKEN: Final = '7701322083:AAGEflh1uPRvVmRj-eAqZKcvsSIWKlBSu20'
+BOT_USERNAME: Final = '@Mr_shotgun_bot'
+
+
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('welcome to mr-_-shotgun')
+    
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('how can i help you')
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('this is custom command')
+
+# Responses
+def handle_response(text: str) -> str:
+    processed: str = text.lower()
+    
+    if 'hello' in processed:
+        return "hiiii"
+    
+    if 'how are you' in processed:
+        return "I am good"
+
+    if 'i love python' in processed:
+        return "me too"
+    
+    return 'sorry i understand'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    
+    print(F'User ({update.message.chat.id}) in {message_type}: "{text}"')
+
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_message(text)
+    
+    print('Bot: ', response)
+    await update.message.reply_text(response)
+
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(F'Update {update} caused error {context.error}')
+
+
+if __name__ == "__main__":
+    print('stating but')
+    app = Application.builder().token(TOKEN).build()
+
+    # Command
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+    
+    # Messages
+    app.add_handler(MessageHandler(filters.Text, handle_message))
+    
+    # Errors
+    
+    app.add_error_handler(error)
+    
+    # polls the bot
+    print('polling...')
+    app.run_polling(poll_interval=3) # update time
